fec0493b12a7fa6b518bd52b297e078a0d70e9e4
Jules Gorce <jules.gorce@gmail.com>
2022-10-30 00:40:23 +0200

    comment broadcast line in Quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,5 +3,9 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
+  # Same as : after_create_commit -> { broadcast_prepend_to "quotes" }
+  # Pas besoin de partial, locals et target car avec les conventions de nommage,
+  # les 3 attributs savent que c'est "quote" dont on parle comme on est dans le model "Quote"
   after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+
 end
